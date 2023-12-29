2c3b8e3c490d073f46ca0fb30fc56f8ae13ad4ed
TAMIRAJA823@GMAIL.COM <TAMIRAJA823@GMAIL.COM>
2023-12-28 23:20:57 -0800

    Database Convention Override

---
+++ Migrations/202312290545536_InitialModel.cs
@@ -0,0 +1,20 @@
+﻿namespace OverConventions.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.TagCourses");
+            AddPrimaryKey("dbo.TagCourses", new[] { "Course_Id", "Tag_Id" });
+        }
+        
+        public override void Down()
+        {
+            DropPrimaryKey("dbo.TagCourses");
+            AddPrimaryKey("dbo.TagCourses", new[] { "Tag_Id", "Course_Id" });
+        }
+    }
+}
---
+++ Migrations/202312290605130_AddedAnotationToCourse.cs
@@ -0,0 +1,20 @@
+﻿namespace OverConventions.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedAnotationToCourse : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Courses", "Title", c => c.String(nullable: false, maxLength: 200));
+            AlterColumn("dbo.Courses", "Description", c => c.String(nullable: false, maxLength: 2000));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Courses", "Description", c => c.String());
+            AlterColumn("dbo.Courses", "Title", c => c.String());
+        }
+    }
+}
